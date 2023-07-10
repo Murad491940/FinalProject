259d66962f298f70eb5002f5bd3bf08204e8ef4a
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-07-10 20:51:31 +0400

    Creating Models

---
+++ FinalProject/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalProject.Enums;
+
+namespace FinalProject.Models
+{
+    class Product
+    {
+        public Product(string name,string surname) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+
+        }
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ProductCategory Ctegory { get; set; }
+        public string ProductCount { get; set; }
+        public string ProductId { get; set; }
+    }
+}
---
+++ FinalProject/Models/Sale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalProject.Enums;
+
+namespace FinalProject.Models
+{
+    class Sale
+    {
+        public Sale(string saleNumber) 
+        { 
+            SaleNumber = saleNumber;
+        }
+        
+        public string SaleNumber { get; set; }
+        public decimal SalePrice { get; set; }
+        public SaleItem İtem { get; set; }   
+        public DateTime SaleTime { get; set; } 
+
+       
+    }
+}
